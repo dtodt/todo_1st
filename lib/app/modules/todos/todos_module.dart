2e379e2a30fc05c636ad279402e61ebf830c7887
dtodt <danieltodt@gmail.com>
2022-02-05 20:10:58 -0300

    unmodifiable map fix

--- lib/app/modules/todos/todos_module.dart
+++ lib/app/modules/todos/todos_module.dart
@@ -1,5 +1,8 @@
+import 'dart:collection';
+
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:todo1st/app/modules/todos/data/datasources/index.dart';
+import 'package:todo1st/app/modules/todos/data/models/index.dart';
 import 'package:todo1st/app/modules/todos/data/repositories/index.dart';
 import 'package:todo1st/app/modules/todos/domain/repositories/index.dart';
 import 'package:todo1st/app/modules/todos/domain/usecases/index.dart';
@@ -9,7 +12,7 @@ import 'package:todo1st/app/modules/todos/presentation/pages/index.dart';
 class TodosModule extends Module {
   @override
   final List<Bind> binds = [
-    Bind<ITodosLocalDS>((_) => const TodosMapDS()),
+    Bind<ITodosLocalDS>((_) => TodosMapDS(map: HashMap<String, TaskModel>())),
     Bind<ITodosRepository>((i) => TodosRepository(i<ITodosLocalDS>())),
     $TodosList,
     $TodosSave,
