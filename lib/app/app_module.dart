8c9799c5de25f138563f466e0c18331dcba83c8e
dtodt <danieltodt@gmail.com>
2022-02-11 22:32:02 -0300

    app module imports and test

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,12 +1,28 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:todo1st/app/modules/todos/todos_module.dart';
+import 'package:todo1st/app/shared/shared_module.dart';
 
 class AppModule extends Module {
   @override
   final List<Bind> binds = [];
 
   @override
-  final List<ModularRoute> routes = [
-    ModuleRoute(Modular.initialRoute, module: TodosModule()),
+  final List<Module> imports = [
+    SharedModule(),
   ];
+
+  @override
+  final List<ModularRoute> routes = [
+    ModuleRoute(Modular.initialRoute,
+        module: TodosModule(), guards: [AppGuard()]),
+  ];
+}
+
+/// TODO find a place to put this guard
+class AppGuard extends RouteGuard {
+  @override
+  Future<bool> canActivate(String path, ModularRoute route) async {
+    await Modular.isModuleReady<AppModule>();
+    return true;
+  }
 }
---
+++ test/app/app_module_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:modular_test/modular_test.dart';
+import 'package:tekartik_app_flutter_sembast/sembast.dart';
+import 'package:todo1st/app/app_module.dart';
+
+void main() {
+  setUpAll(() {
+    initModule(AppModule(), replaceBinds: [
+      Bind<DatabaseFactory>((_) => databaseFactoryMemory, export: true),
+    ]);
+  });
+
+  testWidgets('should setup all dependencies correctly', (_) async {
+    await Modular.isModuleReady<AppModule>();
+
+    final database = Modular.get<Database>();
+    expect(database, isA<Database>());
+
+    // TODO test guard
+  });
+}
